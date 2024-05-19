a7558be17a816538d986291b0e4a6d9ba39a4665
kristisdom <kristupasdomi@gmail.com>
2024-05-19 22:47:05 +0300

    Add files via upload

---
+++ test_1.py
@@ -0,0 +1,25 @@
+import unittest
+from game import *
+
+class test_game(unittest.TestCase):
+    
+    def set_up(self):
+        self.balance = 100 #mock balance
+        self.game = Game(self.balance)
+        self.mock_variables()
+
+    def mock_variables(self):
+        self.x = 1 #mock answer num
+        self.z = 1 #mock answer color
+        self.x_guess = 1 #mock guess num
+        self.z_guess = "red" #mock guess color
+         
+    def test_color_assign(self):
+        y = self.game.answers.color_assign(self.z_guess)
+        self.assertEqual(y, self.z)
+    
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
